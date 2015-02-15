92f827598a4c658318a26c146189ae8465476cda
Petr Mašát <petr.masat@onlio.com>
2015-02-15 13:54:35 +0100

    resetovací css

--- gulpfile.js
+++ gulpfile.js
@@ -5,29 +5,63 @@ var gulp = require('gulp');
 
 // Include Our Plugins
 var jshint = require('gulp-jshint');
-var sass = require('gulp-sass');
+var less = require('gulp-less');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
+var path = require('path');
+var copy = require('gulp-copy');
 
 // Lint Task
+/*
 gulp.task('lint', function() {
 		return gulp.src('www/js/*.js')
 				.pipe(jshint())
 				.pipe(jshint.reporter('default'));
 });
-/*
+
 // Compile Our Sass
-gulp.task('sass', function() {
-		return gulp.src('scss/*.scss')
-			.pipe(sass())
-		.pipe(autoprefixer({
-				browsers: ["last 5 versions", "> 1%", "ie 10", "ie 11", "ie 9", "ie 8"],
+
+gulp.task('less', function() {
+		return gulp.src('elements//.less')
+			.pipe(less())
+			.pipe(autoprefixer({
+				browsers: ["last 3 versions", "> 2%", "ie 10", "ie 11", "ie 9"],
 				cascade: true
 		}))
-			.pipe(gulp.dest('www/css'));
+			.pipe(gulp.dest('work'));
 });
+*/
+
+gulp.task('copyFromBowerLib', function () {
+	gulp.src([
+		'bower_components/normalize.css/normalize.css'
+	])
+	.pipe(gulp.dest('./'));
+});
+
+
+gulp.task('less', function () {
+  gulp.src('./elements/**/*.less')
+	.pipe(less({
+	  paths: [ path.join(__dirname, 'less', 'includes') ]
+	}))
+	.pipe(autoprefixer({
+		browsers: ["last 3 versions", "> 2%", "ie 10", "ie 11", "ie 9"],
+		cascade: true
+	}))
+	.pipe(gulp.dest('./elements/'));
+});
+
+
+gulp.task('concat', function () {
+  gulp.src('elements/**/*.css')
+	.pipe(concat("main.css"))
+	.pipe(gulp.dest('dist/'));
+});
+
+/*
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
@@ -38,13 +72,13 @@ gulp.task('scripts', function() {
 				.pipe(uglify())
 				.pipe(gulp.dest('dist'));
 });
+*/
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-		gulp.watch('www/js/*.js', ['lint', 'scripts']);
-		gulp.watch('scss/*.scss', ['sass']);
+		// gulp.watch('www/js/*.js', ['lint', 'scripts']);
+		gulp.watch('./elements/**/*.less', ['less', 'concat']);
 });
 
 // Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
-*/
+gulp.task('default', ['less', 'concat', 'watch']);

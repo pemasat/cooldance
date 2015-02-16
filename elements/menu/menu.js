83def9ae8f7d81ddb278cda418c3e6d0c891a3db
Petr Mašát <petr.masat@onlio.com>
2015-02-16 23:07:35 +0100

    JS na main menu

--- elements/menu/menu.js
+++ elements/menu/menu.js
@@ -4,16 +4,58 @@ Menu = function(element) {
 	
 	this.element = element;
 	this.phoneControl = null;
+	this.mainMenu = document.getElementById('mainMenu');
+
+	this.isPhoneView = false;
+
+	this.menuToogleEvent = function() {
+		($(me.mainMenu).is(":visible")) ? me.hideMenu() : me.showMenu();
+		return false;
+	};
+
+	this.showMenu = function() {
+		$(me.mainMenu).show();
+	};
+
+	this.hideMenu = function() {
+		$(me.mainMenu).hide();
+	};
+
 
 	this.generateControlForPhone = function() {
 		me.phoneControl = document.createElement('a');
 		me.phoneControl.id = 'menuSwitcher';
 		me.phoneControl.innerHTML = 'Menu';
 		$(me.phoneControl).insertBefore(me.element);
+		$(me.phoneControl).click(me.menuToogleEvent);
+
+		me.isPhoneView = $(me.phoneControl).is(":visible");
+	};
+
+	this.checkVisibilityForPhoneControl = function() {
+		if ( // jedu ze směru mobil -> desktop
+			$(me.phoneControl).is(":visible") === false &&
+			me.isPhoneView === true
+		) {
+			me.showMenu();
+			me.isPhoneView = false;
+		} else if ( // jedu ze směru desktop -> mobil
+			$(me.phoneControl).is(":visible") === true &&
+			me.isPhoneView === false
+		) {
+			me.hideMenu();
+			me.isPhoneView = true;
+		}
+	};
+
+	this.handleDefaultEvents = function() {
+		$(window).resize(me.checkVisibilityForPhoneControl);
 	};
 
 	init = function() {
 		me.generateControlForPhone();
+		me.handleDefaultEvents();
+		(me.isPhoneView) ? me.hideMenu : {};
 	}();
 };
 

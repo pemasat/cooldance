1dd7e4efb35a44dd9ad237fd29c2a9d9186ad1ee
Petr Mašát <petr.masat@onlio.com>
2015-02-23 23:03:44 +0100

    merge

--- dist/all.js
+++ dist/all.js
@@ -6,17 +6,6 @@ $(document).ready(function() {
 				 carouselVisible: 1,
 				 pauseOnHover: true
 		});
-	} else {
-		 $(".carousel").cycle({
-				 speed: 2000,
-				 slides: "div.carousel-item",
-				 carouselVisible: 1,
-				 pauseOnHover: false,
-				 pager: ".carousel-pager",
-				 pagerTemplate: '<span class="slide-links">■</span>',
-				 prev: ".carousel-nav .cycle-prev",
-				 next: ".carousel-nav .cycle-next"
-		});
 
 	}
 });

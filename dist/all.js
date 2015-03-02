1e4587569832c95f59bb49801c62e76c2ba9b8e7
Petr Mašát <petr.masat@onlio.com>
2015-03-02 08:14:01 +0100

    vyhledávání v menu pro mobily

--- dist/all.js
+++ dist/all.js
@@ -59,6 +59,7 @@ Menu = function(element) {
 	this.element = element;
 	this.phoneControl = null;
 	this.mainMenu = document.getElementById('mainMenu');
+	this.searchControl = null;
 
 	this.isPhoneView = false;
 
@@ -86,6 +87,13 @@ Menu = function(element) {
 		me.isPhoneView = $(me.phoneControl).is(":visible");
 	};
 
+	this.addSearchControl = function() {
+		me.searchControl = $().html('<form action="#" method="get" id="searchControl"></form>');
+		$(me.searchControl).append($().html('<input type="search" name="search" />'));
+		$(me.searchControl).append($().html('<input type="submit" value="hledej" />'));
+		$(me.mainMenu).append(me.searchControl);
+	};
+
 	this.checkVisibilityForPhoneControl = function() {
 		if ( // jedu ze směru mobil -> desktop
 			$(me.phoneControl).is(":visible") === false &&
@@ -108,6 +116,7 @@ Menu = function(element) {
 
 	init = function() {
 		me.generateControlForPhone();
+		me.addSearchControl();
 		me.handleDefaultEvents();
 		(me.isPhoneView) ? me.hideMenu : {};
 	}();
